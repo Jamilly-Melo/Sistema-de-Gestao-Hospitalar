52f7c2b6f32090c0c40a0aed78ff6f58a04c5543
Jamilly Melo <jamillymelo027@gmail.com>
2026-07-25 22:37:04 -0300

    Melhoria de comentários

--- sql/etapa2/concorrencia.sql
+++ sql/etapa2/concorrencia.sql
@@ -1,23 +1,12 @@
--- ==========================================================
--- SISTEMA DE GESTÃO HOSPITALAR
--- ETAPA 2 - TESTE DE CONCORRÊNCIA
---
--- Objetivo:
--- Simular duas transações concorrentes tentando escalar
--- o mesmo residente para a mesma data, turno e unidade.
---
--- Estratégia:
--- Lock pessimista com SELECT ... FOR UPDATE.
---
--- IMPORTANTE:
--- Os blocos abaixo devem ser executados em duas sessões
--- separadas do PostgreSQL.
--- ==========================================================
+-- TESTE DE CONCORRÊNCIA
+
+-- Objetivo: simular duas transações concorrentes tentando escalar o mesmo residente para a mesma data, turno e unidade.
+-- Estratégia: lock pessimista com SELECT ... FOR UPDATE.
+
+-- IMPORTANTE: os blocos abaixo devem ser executados em duas sessões separadas do PostgreSQL.
 
 
--- ==========================================================
 -- SESSÃO 1
--- ==========================================================
 
 BEGIN;
 
@@ -41,16 +30,11 @@ VALUES (
     13
 );
 
--- Manter esta transação aberta.
--- Depois que a Sessão 2 ficar bloqueada, executar:
---
--- COMMIT;
+-- Manter esta transação aberta. Depois que a Sessão 2 ficar bloqueada, executar: COMMIT;
 
 
--- ==========================================================
 -- SESSÃO 2
 -- Executar em outro terminal enquanto a Sessão 1 está aberta.
--- ==========================================================
 
 BEGIN;
 
@@ -76,15 +60,12 @@ VALUES (
     13
 );
 
--- A segunda inserção será impedida pela trigger
--- trg_check_sobreposicao_escala.
+-- A segunda inserção será impedida pela trigger trg_check_sobreposicao_escala.
 
 ROLLBACK;
 
 
--- ==========================================================
 -- CONFERÊNCIA FINAL
--- ==========================================================
 
 SELECT COUNT(*) AS quantidade_escalas
 FROM escala
@@ -92,13 +73,10 @@ WHERE id_residente = 8
   AND data_plantao = '2026-09-01'
   AND turno = 'MANHA';
 
--- Resultado esperado:
--- quantidade_escalas = 1
+-- Resultado esperado: quantidade_escalas = 1
 
 
--- ==========================================================
 -- LIMPEZA DO DADO DE TESTE
--- ==========================================================
 
 DELETE FROM escala
 WHERE id_residente = 8
